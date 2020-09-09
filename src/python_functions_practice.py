958fad02678d2360b1822b94ecee10abf732419b
matinmac100 <matinmac100@yahoo.co.uk>
2020-09-09 16:44:25 +0100

    final work

---
+++ src/python_functions_practice.py
@@ -0,0 +1,51 @@
+def return_10():
+    return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(string_1):
+    return len(string_1)
+
+def join_string (string_1, string_2):
+    return (string_1 + string_2) 
+
+def add_string_as_number ( str_1, str_2 ):
+    return (int(str_1) + int(str_2))
+
+months = {
+    1 : "January",
+    2 : "February",
+    3 : "March",
+    4 : "April",
+    5 : "May",
+    6 : "June",
+    7 : "July",
+    8 : "August",
+    9 : "September",
+    10 : "October",
+    11 : "November",
+    12 : "December",
+
+}
+
+def number_to_full_month_name (num_1):
+    return months[num_1]
+
+
+def number_to_full_month_name(num_1):
+    return months[num_1]
+print(number_to_full_month_name(3))
+
+def number_to_full_month_name(num_1):
+    return months[num_1]
+print(number_to_full_month_name(9))
